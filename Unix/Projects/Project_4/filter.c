3b7e2f979526019cb0ae5732740c06960b5f2590
jes57 <jasonstephens1084@gmail.com>
2014-03-19 17:17:39 -0400

    Switched to the copy on my desktop

--- Unix/Projects/Project_4/filter.c
+++ Unix/Projects/Project_4/filter.c
@@ -1,31 +1,31 @@
 
 #include <stdio.h>     /* for convenience */
 #include <stdlib.h>    /* for convenience */
-
-void err_sys1(const char* x) 
-{ 
-    perror(x); 
-    exit(1); 
-}
+#include <string.h>    /* for convenience */
+#include <unistd.h>    /* for convenience */
 
 #define MAXLINE 4096
 
 int 
-main(int argc, char* argv[])
+main(void)
 {
     int n, int1, int2;
     char line[MAXLINE];
 
-    printf("%s\n", "shut it up you!"); 
+    // while ((n=read(STDIN_FILENO, line, MAXLINE)) > 0){
+    //     if(sscanf(line, "%d%d", &int1, &int2)){
+    //         sprintf(line, "%d, %d", int1, int2);
+    //     }
+    // }
 
-/*
+
     while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0) {
         line[n] = 0;    // null terminate 
         if (sscanf(line, "%d%d", &int1, &int2) == 2) {
             if(int1>int2)
-		sprintf(line,"%d\n", int1);
-	    else
-		sprintf(line,"%d\n", int2);
+        sprintf(line,"%d\n", int1);
+        else
+        sprintf(line,"%d\n", int2);
             n=strlen(line);
             if (write(STDOUT_FILENO, line, n) != n)
                 perror("write error");
@@ -33,6 +33,6 @@ main(int argc, char* argv[])
             if (write(STDOUT_FILENO, "invalid args\n", 13) != 13)
                 perror("write error");
         }
-    }*/
+    }
     exit(0);
 }
